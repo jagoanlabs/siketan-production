8c7c2999612b5f8f9b0a3f60eef7b401c3960d6c
Nasrul Fahmi <75nasrulfahmi@gmail.com>
2026-02-26 09:49:23 +0700

    fix: fixing base url to ngawikab

--- frontend/src/service/app-service.ts
+++ frontend/src/service/app-service.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 import { toast } from "sonner";
 
 export const axiosClient = axios.create({
-  // baseURL: import.meta.env.VITE_API_BASE_URL,
-  baseURL: "https://backend-siketan.jagoansatudata.com",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  // baseURL: "https://backend-siketan.jagoansatudata.com",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
